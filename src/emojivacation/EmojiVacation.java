8a5433c8a97fcd95decd4e1ce9c5d16685638e1a
captaincookie886 <45984741+Tehcnalties@users.noreply.github.com>
2025-09-17 15:44:47 -0500

    create mountains and forests

--- src/emojivacation/EmojiVacation.java
+++ src/emojivacation/EmojiVacation.java
@@ -43,15 +43,11 @@ public class EmojiVacation {
 
         addCloudRows(canvas);
 
-        // TODO: [Instructions step 2] Create mountains 50% of the time.
-        //       You should randomly determine the size and number of layers
-        //       (within reasonable constraints).
+        if(percentChance(50)) addMountains(canvas, 400, randomInt(100, 110), randomInt(5, 8));
 
         addGround(canvas, 400);
 
-        // TODO: [Instructions step 2] Create forests 60% of the time. You should randomly
-        //       determine the count for the number of trees. Pick reasonable values for
-        //       other parameters.
+        if(percentChance(60)) addForest(canvas, randomInt(400, 425), randomInt(10, 50), randomInt(5, 35));
 
         List<GraphicsGroup> family = createFamily(2, 3);
         positionFamily(family, 60, 550, 20);
